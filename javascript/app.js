eadca7eda3e8c58de002fc3b4d15390f651bd641
Juan Jose Gonzalez Izquierdo <1144061031@u.icesi.edu.co>
2022-03-14 08:11:13 -0500

    javascript variables

---
+++ javascript/app.js
@@ -0,0 +1,25 @@
+const BASE_URL = "https://icesi.edu.co/api";
+const MAX_NUMBER = 50;
+
+let isOpen = true;
+isOpen = false;
+
+// (+)
+const number1 = 5;
+const number2 = 3;
+const total = number1 + number2;
+
+const userName = "Juan";
+console.log("Bienvenido, " + userName);
+
+console.log(typeof number1);
+console.log(typeof "5");
+
+if (number1 === "5") {
+    console.log("Son iguales!");
+}
+
+if (number2 !== "3") {
+    console.log("Son diferentes!");
+}
+
